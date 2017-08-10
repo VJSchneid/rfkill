45cd5b3e3a3ed38dd200f4f783a53bc53ae1bb10
Viktor Schneider <info@vjs.io>
2017-08-10 23:54:29 +0200

    add test example

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,14 @@
-#include "../rfkill.h"
+#include <rfkill.h>
+#include <iostream>
 
 int main() {
     std::vector<rfkill::Event> events = rfkill::list();
+
+    std::cout << "events:" << std::endl;
+    for (auto &event: events) {
+        std::cout << event.name << std::endl;
+    }
+
+    std::cout << std::endl << "rfkill all!" << std::endl;
+    rfkill::block(rfkill::ALL, true);
 }
